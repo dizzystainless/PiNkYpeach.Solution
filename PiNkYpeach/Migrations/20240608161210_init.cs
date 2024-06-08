fd5c7b34d22c111ca93ac0e3604742307446d6cc
Cecilia <78555434+dizzystainless@users.noreply.github.com>
2024-06-08 19:00:21 +0200

    Webshop start (#5)
    * text loginbutton
    
    * Changed name on project
    
    * Namechange
    
    * Update
    
    ---------
    
    Co-authored-by: Cecilia Önneskog <ceciliaonneskog@gmail.com>

--- PiNkYpeach/Migrations/20240608161210_init.cs
+++ PiNkYpeach/Migrations/20240608161210_init.cs
@@ -3,7 +3,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
-namespace PiNkYpeach.Web.Migrations
+namespace PiNkYpeach.Data.Migrations
 {
     public partial class init : Migration
     {
