799274556ab07c325cb41bb340329cd8b8885e15
Cecilia Önneskog <ceciliaonneskog@gmail.com>
2024-06-10 14:25:56 +0200

    CRUD start

---
+++ PiNkYpeach/Services/Interfaces/IProductService.cs
@@ -0,0 +1,12 @@
+﻿using PiNkYpeach.Api.Data.Entities;
+
+namespace PiNkYpeach.Api.Services.Interfaces
+{
+    public interface IProductService
+    {
+        Task<IEnumerable<Product>> GetItems();
+        Task<IEnumerable<ProductCategory>> GetCategories();
+        Task<Product> GetItem(int id);
+        Task<ProductCategory> GetCategory(int id);
+;    }
+}
---
+++ PiNkYpeach/Services/ProductService.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PiNkYpeach.Api.Data;
+using PiNkYpeach.Api.Data.Entities;
+using PiNkYpeach.Api.Services.Interfaces;
+
+namespace PiNkYpeach.Api.Services
+{
+    public class ProductService : IProductService
+    {
+        private readonly ApplicationContext _context;
+        public ProductService(ApplicationContext context)
+        {
+            _context = context;
+        }
+        public async Task<IEnumerable<ProductCategory>> GetCategories()
+        {
+            var categories = await _context.ProductCategories.ToListAsync();
+            return categories;  
+        }
+
+        public async Task<ProductCategory> GetCategory(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Product> GetItem(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<Product>> GetItems()
+        {
+            var products = await _context.Products.ToListAsync();
+            return products;
+        }
+    }
+}
